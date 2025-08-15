75c57f154eefd03add145f04b268549c9614ca28
Ethan Brand <brandet251@gmail.com>
2025-08-15 19:17:55 +0200

    setup.py update

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     install_requires=[
         "pandas>=2.0",
         "openpyxl>=3.0",
-        "tqdm>=4.60"
+        "tqdm>=4.60",
         "requests>=2.31.0",
     ],
     entry_points={
